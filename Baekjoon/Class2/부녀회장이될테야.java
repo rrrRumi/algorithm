cb60650108dd63d5c69abbada1813e81c443a8d2
rmi <dkfma3323@gmail.com>
2023-03-28 17:11:00 +0900

    2775 부녀회장이 될테야

--- Baekjoon/Class2/부녀회장이될테야.java
+++ Baekjoon/Class2/부녀회장이될테야.java
@@ -5,20 +5,26 @@ import java.util.Scanner;
 public class 부녀회장이될테야 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int answer = 0;
+
+        // APT Setting
+        int[][] apartBlock = new int[15][15];
+        for(int i=0; i<15; i++) {
+            apartBlock[i][1] = 1; // i층 1호
+            apartBlock[0][i] = i; // 0층 i호
+        }
+
+        for(int i=1; i<15; i++) { // 1층 ~ 14층
+            for(int j=2; j<15; j++) { // 2호 ~ 14호
+                apartBlock[i][j] = apartBlock[i-1][j] + apartBlock[i][j-1];
+            }
+        }
+
         int T = in.nextInt(); // Test case
         for(int i=0; i<T; i++) {
-            int k = in.nextInt();
-            int n = in.nextInt();
+            int k = in.nextInt(); // 층
+            int n = in.nextInt(); // 호
 
-            int[][] apartBlock = new int[k][n];
-
-            // 0 Floor Setting
-            for (int apart = n; apart>0; apart++) {
-                apartBlock[0][n] = n+1;
-            }
-
-            System.out.println(answer);
+            System.out.println(apartBlock[k][n]);
         }
     }
 }
